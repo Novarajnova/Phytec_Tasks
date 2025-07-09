da73e404f93a97682dd9e1c4d0af9c23e8d1508b
Novarajnova <novanbr07@gmail.com>
2025-07-09 17:35:24 +0530

    Upload only cleaned WEEK-2 folder without large files

---
+++ WEEK-2/CoAP/coap-example-client/build/nrf52840/dk/obj/.deps/cbor.c.d
@@ -0,0 +1,4 @@
+build/nrf52840/dk/obj/cbor.o: ../../../os/lib/cbor.c \
+ ../../../os/net/app-layer/coap/module-macros.h ../../../os/lib/cbor.h
+../../../os/net/app-layer/coap/module-macros.h:
+../../../os/lib/cbor.h:
---
+++ WEEK-2/CoAP/coap-example-client/build/nrf52840/dk/obj/.deps/syscalls.c.d
@@ -0,0 +1,3 @@
+build/nrf52840/dk/obj/syscalls.o: ../../../os/lib/newlib/syscalls.c \
+ ../../../os/net/app-layer/coap/module-macros.h
+../../../os/net/app-layer/coap/module-macros.h:
